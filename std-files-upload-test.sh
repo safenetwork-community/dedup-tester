8858d14ccd72a766ed9a5277619add6929a4c8d1
Southside <293741+willief@users.noreply.github.com>
2021-05-09 05:52:24 +0100

    Update std-files-upload-test.sh
    Added code to retrieve files
    Improved extraction of account balance
    improved output formatting

--- std-files-upload-test.sh
+++ std-files-upload-test.sh
@@ -10,10 +10,26 @@
 #upload files to SAFE Network and check time elapsed
 #upload checksums for each file to ensure we are dealing with standardised test files
 
-DEST_DIR=/tmp/std-file-upload-tests-02
-PATH=$PATH:/home/$USER/.safe/cli
 
+#period of time to wait  before attempting to retreive the files
+DELAY=60
+
+
+
+DEST_DIR=/tmp/std-file-upload-tests-04
+PATH=$PATH:/home/$USER/.safe/cli         # This should not be needed
+#safe auth restart
+#safe auth create --test-coins
+OPEN_BALANCE=`safe keys balance| safe keys balance|cut -f2 -d':'`
+
+echo "==============================================================="
+echo ""
 echo "output files will be written to " $DEST_DIR
+echo ""
+echo "This account has a balance of "$OPEN_BALANCE
+echo "==============================================================="
+
+
 if [[ -d  $DEST_DIR ]]
     then
     rm -rf $DEST_DIR
@@ -23,27 +39,58 @@ mkdir -p $DEST_DIR && cd $DEST_DIR
 #Get the files  calculate checksum and write to SAFE Network
 
 #----------------------save on AWS bandwith downloads----comment out the curl line and copy in from local disk
-curl --output dedup-testfiles.zip https://maidsafe-t5-dedup-testfiles.s3-eu-west-1.amazonaws.com/dedup-testfiles.zip
+#curl --output dedup-testfiles.zip https://maidsafe-t5-dedup-testfiles.s3-eu-west-1.amazonaws.com/dedup-testfiles.zip
 
-#cp /home/$USER/tmp/testfiles/dedup-testfiles.zip  $DEST_DIR/
+cp /home/$USER/tmp/testfiles/dedup-testfiles.zip  $DEST_DIR/
 #-----------------------------------------------------------------------
 touch safe-xorurl.txt
 unzip dedup-testfiles.zip >/dev/null
 
-for i in 5MB 10MB 20MB #50MB 100MB 200MB   #commented out for speed up dev and test
+for i in 5MB 10MB #20MB #50MB 100MB 200MB   #commented out for speed up dev and test
     do
-        echo "processing a " $i " standard testfile"
+        echo ""
+        echo "processing a "$i" standard testfile"
+        echo ""
         #check integrity of downloaded testfiles
         md5sum $i.zip > calculated-md5-$i.txt
         cmp md5-$i.txt  calculated-md5-$i.txt
         (($? != 0)) && { printf '%s\n' "Checksums did not match.  Aborting!!"; exit 1; }
         #store files to SAFE Network and extract testfiles xorurl address
-        time safe files put $i.zip | tail -c61  >> safe-xorurl.txt
+        time safe files put $i.zip |  cut -f2 -d'"'  >> safe-xorurl.txt
         # /home/$USER/.safe/cli/
-        safe files put md5-$i.txt
+        safe files put md5-$i.txt >>/dev/null
 done
+
+
+CLOSE_BALANCE=`safe keys balance|cut -f2 -d':'`
+echo ""
+echo "========================================================================================"
+echo ""
+echo "cost of storage was " $(($OPEN_BALANCE-$CLOSE_BALANCE))
+echo ""
+echo "sleeping for "$DELAY" seconds"
+sleep $DELAY
+
+
+#---------------------rtreive files-----------------
+
 cat safe-xorurl.txt
+cat safe-xorurl.txt | while read line
+do
+    safe files get safe://$line
+
+done
+
+echo  "TODO      thanks for using this tool  - more to come"
+echo ""
+echo "That's All, Folks!!"
+
+
+
 exit 0
+
+
+
 
 
 
